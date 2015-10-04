a625b2a7314670cae28317bdf3f29ff11b3a7776
Jun Tanami <sechs-053@nifty.com>
2015-10-04 19:56:59 +0900

    接続先を入力して接続することができるところまで

---
+++ src/EV3Controller/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace EV3Controller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Connect = new System.Windows.Forms.Button();
+            this.Send = new System.Windows.Forms.Button();
+            this.Close = new System.Windows.Forms.Button();
+            this.PortNameLabel = new System.Windows.Forms.Label();
+            this.PortNameTaxtBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Connect
+            // 
+            this.Connect.Location = new System.Drawing.Point(206, 32);
+            this.Connect.Name = "Connect";
+            this.Connect.Size = new System.Drawing.Size(75, 23);
+            this.Connect.TabIndex = 20;
+            this.Connect.Text = "Connect";
+            this.Connect.UseVisualStyleBackColor = true;
+            this.Connect.Click += new System.EventHandler(this.Connect_Click);
+            // 
+            // Send
+            // 
+            this.Send.Location = new System.Drawing.Point(87, 78);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(75, 23);
+            this.Send.TabIndex = 30;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = true;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // Close
+            // 
+            this.Close.Location = new System.Drawing.Point(87, 127);
+            this.Close.Name = "Close";
+            this.Close.Size = new System.Drawing.Size(75, 23);
+            this.Close.TabIndex = 40;
+            this.Close.Text = "Close";
+            this.Close.UseVisualStyleBackColor = true;
+            this.Close.Click += new System.EventHandler(this.Close_Click);
+            // 
+            // PortNameLabel
+            // 
+            this.PortNameLabel.AutoSize = true;
+            this.PortNameLabel.Location = new System.Drawing.Point(22, 37);
+            this.PortNameLabel.Name = "PortNameLabel";
+            this.PortNameLabel.Size = new System.Drawing.Size(59, 12);
+            this.PortNameLabel.TabIndex = 3;
+            this.PortNameLabel.Text = "Port Name";
+            // 
+            // PortNameTaxtBox
+            // 
+            this.PortNameTaxtBox.Location = new System.Drawing.Point(87, 34);
+            this.PortNameTaxtBox.Name = "PortNameTaxtBox";
+            this.PortNameTaxtBox.Size = new System.Drawing.Size(100, 19);
+            this.PortNameTaxtBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 262);
+            this.Controls.Add(this.PortNameTaxtBox);
+            this.Controls.Add(this.PortNameLabel);
+            this.Controls.Add(this.Close);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.Connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Connect;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.Button Close;
+        private System.Windows.Forms.Label PortNameLabel;
+        private System.Windows.Forms.TextBox PortNameTaxtBox;
+    }
+}
+
---
+++ src/EV3Controller/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace EV3Controller
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// 画面表示状態
+        /// </summary>
+        enum status
+        {
+            NoConnect ,Connect
+        }
+
+        private SerialPort myPort;
+        private status displayStatus;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// EV3とのBluetooth接続
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Connect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string PortName = this.PortNameTaxtBox.Text;
+                int BaudRate = 9600;
+                Parity Parity = Parity.None;
+                int DataBits = 8;
+                StopBits StopBits = StopBits.One;
+                myPort = new SerialPort(PortName, BaudRate, Parity, DataBits, StopBits);
+                myPort.Open();
+
+                this.displayStatus = status.Connect;
+                changeStatus();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("connect failed.");
+            }
+        }
+
+        /// <summary>
+        /// EV3とのBluetooth切断
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myPort.Close();
+
+                this.displayStatus = status.NoConnect;
+                changeStatus();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("close failed.");
+            }
+        }
+
+        /// <summary>
+        /// EV3への送信(発信指示)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Send_Click(object sender, EventArgs e)
+        {
+            myPort.Write("1");
+        }
+
+        /// <summary>
+        /// 初期表示
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.displayStatus = status.NoConnect;
+            changeStatus();
+        }
+
+        /// <summary>
+        /// 状態遷移
+        /// </summary>
+        private void changeStatus()
+        {
+            switch (this.displayStatus)
+            {
+                case status.NoConnect :
+                    this.Connect.Enabled = true;
+                    this.PortNameTaxtBox.Enabled = true;
+                    this.Send.Enabled = false;
+                    this.Close.Enabled = false;
+                    break;
+                case status.Connect : 
+                    this.Connect.Enabled = false;
+                    this.PortNameTaxtBox.Enabled = false;
+                    this.Send.Enabled = true;
+                    this.Close.Enabled = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
